1974c5a664a76fe09a733e9baf1c53db632aa9f8
alex-lovati <alex.lovati@laplateforme.io>
2021-11-04 17:31:54 +0100

    fin jour4

--- jour3/job01/index.php
+++ jour3/job01/index.php
@@ -1,5 +1,14 @@
 <?php
-$x=array( 200, 204, 173, 98, 171, 404, 459 );
-
+$x=array(200, 204, 173, 98, 171, 404, 459);
+$index=0;
+while ($index<=6){
+if ($x[$index]%2 === 0) {
+    echo ($x[$index]."est pair <br/>");
+}
+else{
+    echo ($x[$index]."est impair <br/>");
+}
+$index++;
+}
 
 ?>
---
+++ jour3/job04/index.php
@@ -0,0 +1,16 @@
+<?php
+$str = "I'm sorry Dave I'm afraid I can't
+do that";
+// $voy = ("a", "e", "i", "o", "u", "y");
+$index=0;
+
+while(isset ($str[$index])){
+    if($str[$index]=="a"or $str[$index]=="e" or $str[$index]=="i" or $str[$index]=="o" or $str[$index]=="u" or $str[$index]=="y" or $str[$index]=="I"){
+        echo ($str[$index]); 
+    }
+    else{
+        echo '';
+    }
+$index++;
+}   
+?>
