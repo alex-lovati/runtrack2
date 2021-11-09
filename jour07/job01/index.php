944e00016dfa038cf4a428195018c4ef5f9eb0e7
alex-lovati <alex.lovati@laplateforme.io>
2021-11-09 13:46:22 +0100

    jour07 job01et02

---
+++ jour07/job01/index.php
@@ -0,0 +1,23 @@
+<?php
+session_start();
+?>
+
+<html>
+
+<?php
+
+if(isset($_SESSION['nbvisite'])){
+    $_SESSION['nbvisite']++;
+}
+if(isset($_POST['reset'])){
+    $_SESSION['nbvisite']=0;
+}
+echo($_SESSION['nbvisite']);
+
+?>
+
+<form method="post" action="index.php">
+    <input type="submit" name="reset" class="button" value="reset" >
+</form>
+
+</html>
